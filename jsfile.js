3ed771b6cc47c42b2225e2885dc526088985668f
Fadi-Aldalati <fadi.aldalati.2510@gmail.com>
2023-01-10 09:10:25 +0300

    half way there

--- jsfile.js
+++ jsfile.js
@@ -1,18 +1,30 @@
-var d = new Date();
-
-        var month = d.getMonth()+1;
-        var day = d.getDate();  
-        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-                    var date = new Date();
-                    var dayName = days[d.getDay()];
-             document.getElementById("day").innerHTML = dayName;
-             document.getElementById("date").innerHTML = date; 
+        const dataTime = (date)=>{
+          let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let ap = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes.toString().padStart(2, '0');
+        let mergeTime = hours + ':' + minutes + ' ' + ap;
+        return mergeTime;
+        }
+    var today = new Date();
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  
+   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                    
+            var dayName = days[today.getDay()];
+             document.getElementById("day").innerHTML = today.toLocaleDateString("en-US", options);
+             document.getElementById("date").innerHTML = dataTime(today);
 
              let lon;
         let lat;
-    
+        var k=1;
+        let u=0;
 const kelvin = 273;
-  
+var temperature ;let summary;let loc ;let icon;let day;
+
+
 window.addEventListener("load", () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -26,7 +38,7 @@ window.addEventListener("load", () => {
       // API URL
       const base =
         `http://api.openweathermap.org/data/2.5/forecast?q=Latakia,Syria&lat=${lat}&` +
-        `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
+        `lon=${lon}&exclude={hourly}&appid=6d055e39ee237af35ca066f35474e9df`;
        
       // Calling the API
       
@@ -35,26 +47,54 @@ window.addEventListener("load", () => {
           return response.json();
         })
         .then((data) => {
-            for(let i=0;i<3;i++){ 
-                console.log(data.list[3].dt_txt[9]);
+            let now =  parseInt(data.list[0].dt_txt[9])+(parseInt(data.list[0].dt_txt[8])*10);
+            
+            console.log(data);
+            for (var i=0;i<5;i++){
+              temperature = document.getElementsByClassName("temp")[i];
+              summary = document.getElementsByClassName("summary")[i];
+              loc = document.getElementsByClassName("location")[i];
+              icon = document.getElementsByClassName("icon")[i];
+              day =days[new Date(data.list[u].dt*1000-(data.city.timezone*1000)).getDay()];
+                
                 temperature.textContent = 
-                    Math.floor(data.list[3].main.temp - kelvin) + "°C";
-                summary.textContent = data.list[3].weather[0].description;
-                loc.textContent = data.city.name + "," + data.city.country;
-                let icon1 = './weather_icons/set01/big/27.png';
+                    Math.floor(data.list[u].main.temp - kelvin) + "°C";
+                summary.textContent = data.list[u].weather[0].description;
+                loc.textContent = day ;/*data.city.name + "," + data.city.country;*/
+                
+                let icon1 = `./icons/${summary.textContent}.png`;
                 icon.innerHTML = 
                     `<img src="${icon1}" />`;
+                   if(k<0)  u +=8 ;
+                       
+                   
+                    while(k>0){
+                        if(parseInt(data.list[k].dt_txt[9])+(parseInt(data.list[k].dt_txt[8])*10) > now){
+                            u = k;
+                            k=-3;
 
-            }   
+                        }
+                        k++;
+
+                    }
+                }
         });
     });
   }
 });
+
+
+/*for (var i=0;i<5;i++){
+temperature = document.getElementsByClassName("temp")[i];}*/
+
+
+
+
+/*
+var x ="";
+x = <
 let temperature = document.querySelector(".temp");
 let summary = document.querySelector(".summary");
 let loc = document.querySelector(".location");
 let icon = document.querySelector(".icon");
-/*
-var x ="";
-x = <
 */
